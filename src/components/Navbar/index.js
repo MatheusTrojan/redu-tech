2d4306e52bf14009d653c637bf01f12ddd5eec87
Matheus Trojan <matheustrojan23@gmail.com>
2023-02-03 11:53:34 -0300

    inicia criação do navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./Navbar.module.scss"
+import { ReactComponent as Logo } from "../../assets/logo.svg"
+
+export default function Navbar() {
+  return (
+    <nav className={styles.nav}>
+        <Logo className={styles.logo}/>
+    </nav>
+  )
+}
