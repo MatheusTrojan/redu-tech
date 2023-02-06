2ed76074015b5eea705cb421845cd03643bba4c4
Matheus Trojan <matheustrojan23@gmail.com>
2023-02-06 16:42:56 -0300

    edita os tipos de categorias

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,35 +1,42 @@
 import Header from "components/Header"
 import styles from "./Home.module.scss"
-import logosCursos from "assets/inicial.png"
+import logosCursos from "assets/inicial.jpg"
+
+import frontThumb from "assets/categorias/thumbnail/front-end.png"
+import devOpsThumb from "assets/categorias/thumbnail/devops.png"
+import UxThumb from "assets/categorias/thumbnail/ux-design.png"
+import dataScienceThumb from "assets/categorias/thumbnail/data-science.png"
+import mobileThumb from "assets/categorias/thumbnail/mobile.png"
+
 import { useNavigate } from "react-router-dom";
 
 const categorias =  [{
-  nome: 'Eletrônicos',
-  thumbnail: "eletronicosThumb",
+  nome: 'Front-end',
+  thumbnail: frontThumb,
   header: "eletronicosHeader",
   id: 'eletronicos',
   descricao: 'Os melhores e mais atuais dispositivos eletrônicos estão aqui!'
 }, {
-  nome: 'Automotivo',
-  thumbnail: "automotivoThumb",
+  nome: 'DevOps',
+  thumbnail: devOpsThumb,
   header: "automotivoHeader",
   id: 'automotivos',
   descricao: 'Encontre aqui equipamentos para deixar seu carro com a sua cara!'
 }, {
-  nome: 'Jogos',
-  thumbnail: "jogosThumb",
+  nome: 'UX & Design',
+  thumbnail: UxThumb,
   header: "jogosHeader",
   id: 'jogos',
   descricao: 'Adquira os consoles e jogos mais atuais do mercado !'
 }, {
-  nome: 'Escritório',
-  thumbnail: "escritorioThumb",
+  nome: 'Data Science',
+  thumbnail: dataScienceThumb,
   header: "escritorioHeader",
   id: 'escritorio',
   descricao: 'Tudo para o que escritório ficar incrível!'
 }, {
-  nome: 'Som e imagem',
-  thumbnail: "somThumb",
+  nome: 'Mobile',
+  thumbnail: mobileThumb,
   header: "somHeader",
   id: 'som',
   descricao: 'Curta suas músicas e seus filmes com a melhor qualidade!'
@@ -58,7 +65,7 @@ export default function Home() {
           {categorias.map((categoria, index) => (
             <div key={index} onClick={() => navigate(`/categorias/${categoria.id}`)}>
               <img src={categoria.thumbnail} alt={categoria.nome} />
-              <h1>{categoria.nome}</h1>
+              <h2>{categoria.nome}</h2>
             </div>
           ))}
 
