644ab3bea1eb9df23bdbf85cb26cbc98982457ff
Matheus Trojan <matheustrojan23@gmail.com>
2023-02-06 17:45:27 -0300

    cria store e primeiro reducer

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,11 @@
 import styles from "./Header.module.scss"
 
-export default function Header({ titulo, descricao, className = "", imagem}) {
+export default function Header({ titulo, descricao, className = ""}) {
   return (
     <header className={`${styles.header} ${className}`}>
         <div className={styles["header-texto"]}>
             <h1>{titulo}</h1>
             <h2>{descricao}</h2>
-        </div>
-
-        <div className={styles["header-imagem"]}>
-            <img 
-                alt={titulo}
-                src={imagem}
-            />
         </div>
     </header>
   )
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,50 +1,14 @@
 import Header from "components/Header"
 import styles from "./Home.module.scss"
 import logosCursos from "assets/inicial.jpg"
-
-import frontThumb from "assets/categorias/thumbnail/front-end.png"
-import devOpsThumb from "assets/categorias/thumbnail/devops.png"
-import UxThumb from "assets/categorias/thumbnail/ux-design.png"
-import dataScienceThumb from "assets/categorias/thumbnail/data-science.png"
-import mobileThumb from "assets/categorias/thumbnail/mobile.png"
-
 import { useNavigate } from "react-router-dom";
-
-const categorias =  [{
-  nome: 'Front-end',
-  thumbnail: frontThumb,
-  header: "eletronicosHeader",
-  id: 'eletronicos',
-  descricao: 'Os melhores e mais atuais dispositivos eletrônicos estão aqui!'
-}, {
-  nome: 'DevOps',
-  thumbnail: devOpsThumb,
-  header: "automotivoHeader",
-  id: 'automotivos',
-  descricao: 'Encontre aqui equipamentos para deixar seu carro com a sua cara!'
-}, {
-  nome: 'UX & Design',
-  thumbnail: UxThumb,
-  header: "jogosHeader",
-  id: 'jogos',
-  descricao: 'Adquira os consoles e jogos mais atuais do mercado !'
-}, {
-  nome: 'Data Science',
-  thumbnail: dataScienceThumb,
-  header: "escritorioHeader",
-  id: 'escritorio',
-  descricao: 'Tudo para o que escritório ficar incrível!'
-}, {
-  nome: 'Mobile',
-  thumbnail: mobileThumb,
-  header: "somHeader",
-  id: 'som',
-  descricao: 'Curta suas músicas e seus filmes com a melhor qualidade!'
-}];
+import { useSelector } from "react-redux";
 
 export default function Home() {
 
   const navigate = useNavigate();
+
+  const categorias = useSelector(state => state.categorias);
 
   return (
     <div>
@@ -52,12 +16,11 @@ export default function Home() {
         titulo="Classificados Tech"
         descricao="Adquira os melhores cursos de programação disponíveis no mercado!"
         className={styles.header}
-        imagem={logosCursos}
       />
       <div className={styles.categorias}>
         <div className={styles["categorias-title"]}>
           <h1>
-            Categorias
+            Confira as categorias:
           </h1>
         </div>
 
