88918cb38f243d39ded555976bc018491654ac4e
Matheus Trojan <matheustrojan23@gmail.com>
2023-02-07 14:57:24 -0300

    adiciona itens no carrinho

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -4,6 +4,7 @@ import { FaCartPlus } from "react-icons/fa"
 import { mudarFavorito } from 'store/reducers/itens';
 import { useDispatch, useSelector } from "react-redux"
 import { mudarCarrinho } from 'store/reducers/carrinho';
+import classNames from 'classnames';
 
 
 const iconeProps = {
@@ -19,7 +20,8 @@ export default function Item(props) {
         preco,
         descricao,
         favorito,
-        id
+        id,
+        carrinho
     } = props;
 
     const dispatch = useDispatch();
@@ -34,7 +36,9 @@ export default function Item(props) {
     }
 
     return (
-        <div className={styles.item}>
+        <div className={classNames(styles.item, {
+            [styles.itemNoCarrinho]: carrinho
+        })}>
             <div className={styles["item-imagem"]}>
                 <img src={foto} alt={titulo} />
             </div>
